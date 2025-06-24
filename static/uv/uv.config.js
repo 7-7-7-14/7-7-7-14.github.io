c90b282f91f0fa7d8dfaa140a7dae68d883b1207
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-24 21:06:27 +0100

    working

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://math.cinevez.lol/bare/',
+    bare:'https://void.radio.fm/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
