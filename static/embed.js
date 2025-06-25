342714112328fd20162adb99adcc99bd031cddc7
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 03:57:35 +0100

    working

--- static/embed.js
+++ static/embed.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('load', () => {
   const link = new URLSearchParams(window.location.search).get('p');
-  if (link) {
-    if (link.startsWith("https://play.geforcenow.com")) {
-      self.__uv$config.bare = "https://infrared.zrhe2016.workers.dev/";
-    } else {
-      self.__uv$config.bare = 'https://incog.dev/bare/';
-    }
-    window.location.href = `${self.__uv$config.prefix}${self.__uv$config.encodeUrl(link)}`;
+  if (!link) return;
+
+  if (link.startsWith("https://play.geforcenow.com")) {
+    self.__uv$config.bare = "https://infrared.zrhe2016.workers.dev/";
+  } else {
+    self.__uv$config.bare = 'https://incog.dev/bare/';
   }
+
+  const encoded = self.__uv$config.prefix + Ultraviolet.codec.xor.encode(link);
+  console.log('Redirecting to:', encoded);
+  window.location.href = encoded;
 });
