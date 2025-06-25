181f92e80efc75d9d3890cdd30675504cb6cf888
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 04:49:04 +0100

    void?

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://backend.infrared.bomberfish.ca/',
+    bare:' https://void.radio.fm/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
