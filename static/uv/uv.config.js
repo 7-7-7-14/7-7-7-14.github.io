c9f8d02907822f733b84df8ec009a34d36be2702
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 15:55:33 +0100

    🥳

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://void.radio.fm/bare/',
+    bare:'https://infrared.zrhe2016.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -9,7 +9,7 @@ self.__uv$config = {
     sw: '/static/uv/uv.sw.js',
 }
 
-// https://void.radio.fm/bare/ - mid, loads gf
+// https://void.radio.fm/bare/ - mid, loads gf, blocked by CORS!!!
 // https://backend.infrared.bomberfish.ca/ - gf doesnt load
 // https://infrared.zrhe2016.workers.dev/ - gf doesnt load
 // https://incog.dev/bare/ - fast, blocked by CORS
