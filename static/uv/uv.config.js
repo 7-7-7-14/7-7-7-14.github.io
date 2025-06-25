e9bb5accbd3735f34a66bca412207839ff480b19
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 04:59:19 +0100

    take 3

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://bare.commander.whimsy.me/',
+    bare:'https://void.radio.fm/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -9,8 +9,8 @@ self.__uv$config = {
     sw: '/static/uv/uv.sw.js',
 }
 
-// https://void.radio.fm/bare/ - slow, loads gf
+// https://void.radio.fm/bare/ - mid, loads gf
 // https://backend.infrared.bomberfish.ca/ - gf doesnt load
 // https://infrared.zrhe2016.workers.dev/ - gf doesnt load
 // https://incog.dev/bare/ - fast, blocked by CORS
-// https://bare.commander.whimsy.me/
+// https://bare.commander.whimsy.me/ - doenst load gf assets
