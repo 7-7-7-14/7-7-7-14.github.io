1d774e2fba119cfc840321f7ca28bf656edfacd3
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 03:59:13 +0100

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const link = new URLSearchParams(window.location.search).get('p');
   if (link) {
-    if (link.startsWith("https://play.geforcenow.com")) {
-      self.__uv$config.bare = "https://infrared.zrhe2016.workers.dev/";
-    } else {
-      self.__uv$config.bare = 'https://incog.dev/bare/';
-    }
-    window.location.href = `${self.__uv$config.prefix}${self.__uv$config.encodeUrl(link)}`;
+    window.location.href = /static/load/${__uv$config.encodeUrl(link)};
   }
 });
