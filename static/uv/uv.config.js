afbbb75eb8eabd182e77da15bfd0d7e3fc54256b
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 04:51:29 +0100

    fixed 🥳

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -9,8 +9,8 @@ self.__uv$config = {
     sw: '/static/uv/uv.sw.js',
 }
 
-// https://void.radio.fm/bare/
-// https://backend.infrared.bomberfish.ca/
-// https://infrared.zrhe2016.workers.dev/ - works w geforce
+// https://void.radio.fm/bare/ - slow, loads gf
+// https://backend.infrared.bomberfish.ca/ - gf doesnt load
+// https://infrared.zrhe2016.workers.dev/ - gf doesnt load
 // https://incog.dev/bare/ - fast, blocked by CORS
-//
+// https://bare.commander.whimsy.me/
