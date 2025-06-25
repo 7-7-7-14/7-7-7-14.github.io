d2dc656eac12421c0a54e0014fea9c2d5a363701
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 16:26:28 +0100

    bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://backend.infrared.bomberfish.ca/',
+    bare:'https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -14,3 +14,4 @@ self.__uv$config = {
 // https://infrared.zrhe2016.workers.dev/ - gf doesnt load
 // https://incog.dev/bare/ - fast, blocked by CORS
 // https://bare.commander.whimsy.me/ - doenst load gf assets
+// https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/
