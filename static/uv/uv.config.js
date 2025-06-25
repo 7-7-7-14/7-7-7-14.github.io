6cab22c8191dbafb43cd1acf188fa49f9ee201e4
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 04:57:17 +0100

    reccoment void

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:' https://void.radio.fm/bare/',
+    bare:'https://bare.commander.whimsy.me/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
