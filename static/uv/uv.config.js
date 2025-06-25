f9ffd7556616d3947786e7fb8c5683c09c913eb7
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-25 04:20:05 +0100

    config

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://infrared.zrhe2016.workers.dev/',
+    bare:'https://incog.dev/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
