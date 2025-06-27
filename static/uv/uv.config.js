7e3ff2de3ad962d10b82e5972da1e87884a84cee
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-27 11:08:30 +0100

    added

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -16,6 +16,6 @@ self.__uv$config = {
 // https://bare.commander.whimsy.me/ - doenst load gf assets
 // https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/ - no connect to gfnow servers
 // https://dm-unbl0cker-bare-server-dusky.vercel.app/
-// https://bare-2wn.pages.dev/
+// https://bare-2wn.pages.dev/ - blocked in school
 // https://koyeb.koyeb.app/fq/ laggy ah geforce 100ms ping better than nothing
 // https://mexi.rest/bare/ ts so slow 🥀
