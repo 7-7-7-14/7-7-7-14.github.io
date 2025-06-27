ff9af766f4666f0015fc4f070b59f02a9e6de5d8
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-27 18:54:10 +0100

    no b64

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'aHR0cHM6Ly9tZXhpLnJlc3QvYmFyZS8=',
+    bare:'https://mexi.rest/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
