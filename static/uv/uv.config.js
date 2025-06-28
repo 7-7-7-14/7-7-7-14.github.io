9bc24f63e4a4f035a4b6ff9bbbc687107d6db81e
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-28 20:40:07 +0100

    changes bare like th 100th time

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://login.i-ready.com.de/bare/',
+    bare:'https://mexi.rest/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
