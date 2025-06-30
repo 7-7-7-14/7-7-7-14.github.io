78ab2043d4f9accfe5df28652238a97d5ec88d1f
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-30 15:40:08 +0100

    fix

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://equinox-beta-one.vercel.app/bare/',
+    bare:'https://equinox-beta-one.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
