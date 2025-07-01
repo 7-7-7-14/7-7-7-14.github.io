424ba3e34cbf3013a3b260757abe1a8b46d588b8
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-01 09:51:29 +0100

    config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://bare.koyeb.app/fq/',
+    bare:'https://searchequinox.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -8,16 +8,3 @@ self.__uv$config = {
     config: '/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 }
-
-// https://void.radio.fm/bare/ - mid, loads gf, blocked by CORS!!!
-// https://backend.infrared.bomberfish.ca/ - gf doesnt load
-// https://infrared.zrhe2016.workers.dev/ - gf doesnt load
-// https://incog.dev/bare/ - fast, blocked by CORS
-// https://bare.commander.whimsy.me/ - doenst load gf assets
-// https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/ - no connect to gfnow servers
-// https://dm-unbl0cker-bare-server-dusky.vercel.app/
-// https://bare-2wn.pages.dev/ - blocked in school
-// https://koyeb.koyeb.app/fq/ laggy ah geforce 100ms ping better than nothing
-// https://mexi.rest/bare/ - service is down
-// https://searchequinox.com/bare/ slower than mexi only gfnow tho
-// https://equinox-beta-one.vercel.app/bare/ - my one 
