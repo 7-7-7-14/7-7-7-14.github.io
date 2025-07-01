e680496ef447712e09a9ebbb40cf9ff99b4ce666
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-01 17:24:20 +0100

    better bare ig

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://backend.infrared.bomberfish.ca/',
+    bare:'https://broad-bird-855e.16jakinyanju.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -8,9 +8,3 @@ self.__uv$config = {
     config: '/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 }
-
-// aHR0cHM6Ly9iYWNrZW5kLmluZnJhcmVkLmJvbWJlcmZpc2guY2Ev
-// https://bare.commander.whimsy.me/ - doenst load gf assets
-// https://koyeb.koyeb.app/fq/ laggy ah geforce 100ms ping better than nothing
-// https://searchequinox.com/bare/ slower than mexi only gfnow tho
-// https://equinox-beta-one.vercel.app/bare/ - my one 
