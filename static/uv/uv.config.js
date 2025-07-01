5d6d36da975ccc4c00f14bea6ad36562b0199a29
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-01 10:03:09 +0100

    fix?

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://searchequinox.com/bare/',
+    bare:'https://backend.infrared.bomberfish.ca/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
@@ -8,3 +8,11 @@ self.__uv$config = {
     config: '/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 }
+
+// aHR0cHM6Ly9iYWNrZW5kLmluZnJhcmVkLmJvbWJlcmZpc2guY2Ev
+// https://bare.commander.whimsy.me/ - doenst load gf assets
+// https://bare-2wn.pages.dev/ - blocked in school
+// https://koyeb.koyeb.app/fq/ laggy ah geforce 100ms ping better than nothing
+// https://mexi.rest/bare/ - service is down
+// https://searchequinox.com/bare/ slower than mexi only gfnow tho
+// https://equinox-beta-one.vercel.app/bare/ - my one 
