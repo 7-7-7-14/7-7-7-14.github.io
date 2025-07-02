1a8b584304f9db9dfa9c110b39a1613684685d89
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-02 17:56:33 +0100

    Create rndm.js

---
+++ storage/js/rndm.js
@@ -0,0 +1,19 @@
+{
+	"versions": [
+		"v1",
+		"v2",
+		"v3"
+	],
+	"language": "Cloudflare",
+	"maintainer": {
+		"email": "no",
+		"website": "https://skibidi.cfd/"
+	},
+	"project": {
+		"name": "Static CNFG",
+		"description": "(Static) TompHTTP-compliant proxy server running on Cloudflare Workers.",
+		"website": "https://nexus-sub.netlify.app",
+		"repository": "https://github.com/breezly101/Nexus",
+		"version": "1.0.0"
+	}
+}
