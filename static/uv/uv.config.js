14ca8ff59708a0e82de3f75324966685bc19b6a9
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-04 10:53:28 +0100

    changed bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://ojojojojojoj.vercel.app/bare/', // https://infrared.zrhe2016.workers.dev/, https://skibidi.cfd/bare/, https://zc.fishoho.com/bare/, https://ojojojojojoj.vercel.app/bare/
+    bare:'https://infrared.zrhe2016.workers.dev/', // https://infrared.zrhe2016.workers.dev/, https://zc.fishoho.com/bare/, https://ojojojojojoj.vercel.app/bare/
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
