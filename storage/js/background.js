5b724d2fec4f3740b90c277c22e7ffb2f748dbb0
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-05 15:54:08 +0100

    bg

--- storage/js/background.js
+++ storage/js/background.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
     const applyBackgroundColor = () => {
-        const color = localStorage.getItem('backgroundColor') || '#ffffff';
+        const color = localStorage.getItem('backgroundColor') || '#0d0d0d';
         if (document.body.style.backgroundColor !== color) {
             document.body.style.backgroundColor = color;
         }
