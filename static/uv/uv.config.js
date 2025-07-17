4b4b58e1e0409ff92f45c023d8bab1a966e8b2f3
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-17 15:36:49 +0100

    bar

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://we.internet-slackers.us/bare/', // https://dogeub-v4-53xc.vercel.app/seal/, https://r2fh2387hv2b2rfbiz.site
+    bare:'https://v2202412246404304352.megasrv.de/bare/', // https://dogeub-v4-53xc.vercel.app/seal/, https://r2fh2387hv2b2rfbiz.site, https://v2202412246404304352.megasrv.de/bare/,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
