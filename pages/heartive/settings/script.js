4403741ffcba5a1f9a9001427b318a3ac02a3671
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-21 19:35:29 +0100

    not working

--- pages/heartive/settings/script.js
+++
@@ -1,1 +0,0 @@
-let tmdbQuality=document.getElementById("tmdb-image-quality"),tmdbLanguage=(tmdbQuality.value=getTmdbImageQuality(),tmdbQuality.addEventListener("change",()=>{setTmdbImageQuality(tmdbQuality.value)}),document.getElementById("tmdb-language"));tmdbLanguage.value=getTmdbLanguage(),tmdbLanguage.addEventListener("change",()=>{setTmdbLanguage(tmdbLanguage.value)});
