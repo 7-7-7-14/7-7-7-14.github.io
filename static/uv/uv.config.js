05de4dca9de0339bed57b1a66e7df0e9453097e4
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-24 15:24:31 +0100

    test to see fastest bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://when.casaye.com/bare/', // https://astroid.wtf/bare/, https://r2fh2387hv2b2rfbiz.site, https://when.casaye.com/bare/, https://bare.koyeb.app/
+    bare:'https://bare2.mysticmath.workers.dev', // https://astroid.wtf/bare/, https://r2fh2387hv2b2rfbiz.site, https://when.casaye.com/bare/, https://bare.koyeb.app/
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
