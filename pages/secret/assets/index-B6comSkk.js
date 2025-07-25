ff74fdccd1d573d47e2ec85124835457b3886e9a
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-25 18:10:32 +0100

    adding videos later

--- pages/secret/assets/index-B6comSkk.js
+++ pages/secret/assets/index-B6comSkk.js
@@ -1,4 +1,44 @@
-(function(){const l=document.createElement("link").relList;if(l&&l.supports&&l.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const d of r.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&n(d)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();function c(t){t.addEventListener("click",()=>{const l=document.querySelector("#container"),i=document.querySelector("video");document.querySelector("#video-overlay").classList.toggle("hidden"),l.classList.remove("hidden"),i.play()})}const a=["1.mp4","2.mp4","3.mp4","4.mp4","5.mp4","6.mp4","7.mp4"];function u(t){return`https://pub-f781e2b2eec341c592ca15670ba4ded0.r2.dev/${t}`}function p(t){const l=Math.floor(Math.random()*t.length);return u(t[l])}const f=p(a);document.querySelector("#app").innerHTML=`
+(function() {
+    const l = document.createElement("link").relList;
+    if (l && l.supports && l.supports("modulepreload")) return;
+    for (const e of document.querySelectorAll('link[rel="modulepreload"]')) n(e);
+    new MutationObserver(e => {
+        for (const r of e)
+            if (r.type === "childList")
+                for (const d of r.addedNodes) d.tagName === "LINK" && d.rel === "modulepreload" && n(d)
+    }).observe(document, {
+        childList: !0,
+        subtree: !0
+    });
+
+    function i(e) {
+        const r = {};
+        return e.integrity && (r.integrity = e.integrity), e.referrerPolicy && (r.referrerPolicy = e.referrerPolicy), e.crossOrigin === "use-credentials" ? r.credentials = "include" : e.crossOrigin === "anonymous" ? r.credentials = "omit" : r.credentials = "same-origin", r
+    }
+
+    function n(e) {
+        if (e.ep) return;
+        e.ep = !0;
+        const r = i(e);
+        fetch(e.href, r)
+    }
+})();
+
+function c(t) {
+    t.addEventListener("click", () => {
+        const l = document.querySelector("#container"),
+            i = document.querySelector("video");
+        document.querySelector("#video-overlay").classList.toggle("hidden"), l.classList.remove("hidden"), i.play()
+    })
+}
+const a = ["1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4", "6.mp4", "7.mp4"];
+
+function p(t) {
+    const l = Math.floor(Math.random() * t.length);
+    return t[l]; 
+}
+const f = p(a);
+document.querySelector("#app").innerHTML = `
   <div id="loading-screen" class="hidden absolute w-full h-full lg:flex justify-center items-center bg-video z-20">
     <p class="text-white text-lg">Loading...</p>
   </div>
@@ -29,6 +69,20 @@
       </div>
     </div>
   </div>
-`;const o=document.getElementById("video-player"),s=document.getElementById("loading-screen");o.addEventListener("loadedmetadata",()=>{s.style.display="none"});o.addEventListener("ended",()=>{o.style.cursor="pointer",o.addEventListener("click",function t(){o.play(),o.style.cursor="default",o.removeEventListener("click",t)})});c(document.querySelector("#play"));o.addEventListener("error",t=>{console.error("Error loading video:",t),s.innerHTML=`
+`;
+const o = document.getElementById("video-player"),
+    s = document.getElementById("loading-screen");
+o.addEventListener("loadedmetadata", () => {
+    s.style.display = "none"
+});
+o.addEventListener("ended", () => {
+    o.style.cursor = "pointer", o.addEventListener("click", function t() {
+        o.play(), o.style.cursor = "default", o.removeEventListener("click", t)
+    })
+});
+c(document.querySelector("#play"));
+o.addEventListener("error", t => {
+    console.error("Error loading video:", t), s.innerHTML = `
     <p class="text-white text-lg">Error loading video. Please try again.</p>
-  `,s.style.display="flex"});
+  `, s.style.display = "flex"
+});
