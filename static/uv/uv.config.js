1287b029267ee7bdbe84eb4546489fdd1dffeeea
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-07-28 17:32:37 +0100

    new bare gng

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://bare2.mysticmath.workers.dev', // https://astroid.wtf/bare/, https://r2fh2387hv2b2rfbiz.site, https://when.casaye.com/bare/, https://bare.koyeb.app/
+    bare:'https://play.szvy.win/bare/', // https://astroid.wtf/bare/, https://r2fh2387hv2b2rfbiz.site, https://when.casaye.com/bare/, https://bare2.mysticmath.workers.dev
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
