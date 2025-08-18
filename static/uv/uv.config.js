8208adea33cbb414a16d520f318264fde1631ed2
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-08-18 22:59:06 +0100

    coding is so hard man 😭🙏🥀

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://trigonometry.texasmath.net/spiders/', // https://astroid.wtf/bare/, https://r2fh2387hv2b2rfbiz.site, https://when.casaye.com/bare/, https://bare2.mysticmath.workers.dev, https://play.szvy.win/bare/
+    bare:'https://play.szvy.win/bare/', // https://astroid.wtf/bare/, https://trigonometry.texasmath.net/spiders/, https://when.casaye.com/bare/, https://bare2.mysticmath.workers.dev, https://play.szvy.win/bare/
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
