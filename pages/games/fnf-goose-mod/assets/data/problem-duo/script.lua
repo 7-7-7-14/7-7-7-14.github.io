b532f37c4642429bf2f736d5dd57987f0cfd6cb0
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-08-19 19:15:16 +0100

    Delete pages/games/fnf-goose-mod directory

--- pages/games/fnf-goose-mod/assets/data/problem-duo/script.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	setPropertyFromClass('ClientPrefs', 'middleScroll', true);
-	triggerEvent('Camera Follow Pos', '1165', '400')
-end
-function onDestroy()
-	setPropertyFromClass('ClientPrefs', 'middleScroll', false);
-end
