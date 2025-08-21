da1f0908edb440960b8a565c9d4f7f81cd6efb01
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-08-21 18:39:40 +0100

    forgot encode links

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,8 @@
 self.__uv$config = {
     prefix: '/static/load/',
     bare:'https://trigonometry.texasmath.net/spiders/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
     bundle: '/static/uv/uv.bundle.js',
     config: '/static/uv/uv.config.js',
